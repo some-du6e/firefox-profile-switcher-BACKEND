e723cf1269fe48a35bde7f5055b36f1fb2722249
Abdelkarim Amro Eltaib Molina <103129948+some-du6e@users.noreply.github.com>
2024-12-03 11:40:27 -0600

    do stuff

--- setup.py
+++ setup.py
@@ -11,3 +11,13 @@ print("moving file to /etc/")
 os.system("sudo mkdir /etc/firefox-profile-switcher > /dev/null 2>&1")
 os.system("sudo cp ./main.py /etc/firefox-profile-switcher/main.py > /dev/null 2>&1")
 print("done")
+prompt = input("mac or linux? (CASE SENSITIVE) ")
+
+if prompt == "linux":
+    print("chose linux")
+    os.system("sudo cp ./firefox_profile_switcher.json /usr/lib/mozilla/native-messaging-hosts/firefox_profile_switcher.json > /dev/null")
+    print("done")
+    print('\a')
+    print("RESTART FIREFOX!!!!")
+else:
+    print("lmao ur cooked")
